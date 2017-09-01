9fe6e5f8821a51cee323bfa3ed21eb248fcefca7
KeeganHansen123 <kh311508@students.davenport.k12.ia.us>
2017-09-01 08:24:57 -0500

    update to new folder

---
+++ Assessment/src/standards/test1.java
@@ -0,0 +1,13 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class test1 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Ya like Jazz");
+	}
+
+}
---
+++ Assessment/src/standards/test3.java
@@ -0,0 +1,15 @@
+package standards;
+
+import javax.swing.JOptionPane;
+
+public class test3 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		//System.out.println("This is for the test");
+		JOptionPane.showMessageDialog(null, "This is for the test");
+	}
+
+}
